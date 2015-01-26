e506f5c38bdcce6e974b67158561efd1804480bc
takatsugu ishikawa <tissi0708@gmail.com>
2015-01-27 00:49:43 +0900

    add a test case

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -5,13 +5,19 @@ describe 'My behaviour' do
 
   it 'should do something' do
 
-    true.should == true
+    expect(true).to eq(true)
   end
 end
 
 RSpec.describe "A calc" do
+  before do
+    @calc = Calc.new
+  end
   it "given 2 and 3 returns 5" do
-    calc = Calc.new
-    expect(calc.add(2, 3)).to eq(5)
+    expect(@calc.add(2, 3)).to eq(5)
+  end
+  it "given 5 and 8 returns 13" do
+    expect(@calc.add(5, 8)).to eq(13)
   end
 end
+
